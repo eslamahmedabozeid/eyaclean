a54a1aa93cf6b8d31da679bce7a0f10a57333d3f
eslamahmedabozei <your.email@example.com>
2024-05-18 19:45:12 +0300

    react

--- app/component/navbar/Navbar.tsx
+++ app/component/navbar/Navbar.tsx
@@ -1,17 +1,30 @@
 import React from 'react'
 import Image from 'next/image'
+import  './styleNav.css'
+import {Container, Row, Col} from 'react-bootstrap';
+
 
 function Navbar() {
   return (
     <div>
-      <ul className='nav_bar'>
-        <li>تواصل معنا</li>
-        <li>سياسة الخصوصية</li>
-        <li>سياسة الاسترجاع</li>
-      </ul>
+      <Container>
+      <div className="flexed">
+        <ul className='nav_bar'>
+          <li>تواصل معنا</li>
+          <li>سياسة الخصوصية</li>
+          <li>سياسة الاسترجاع</li>
+        </ul>
+      <div className="langu">
+        <p>الإنجليزية</p>
+      </div>
+      </div>
+      </Container>
+
       <div className='logo'>
         <Image src="/logo.png" width={500} height={500} alt="Picture of the author"/>
       </div>
+
+
     </div>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import Navbar from "./component/navbar/Navbar";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 export default function Home() {
