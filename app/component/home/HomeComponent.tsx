a0ae1df0fc81b63f9b0042888ec97875d5b6c9fc
eslamahmedabozei <your.email@example.com>
2024-05-18 19:56:43 +0300

    react

--- app/component/home/HomeComponent.tsx
+++ app/component/home/HomeComponent.tsx
@@ -1,10 +1,24 @@
 import React from 'react'
 import './styleHome.css'
+import {Container, Row, Col} from 'react-bootstrap';
+import Image from 'next/image';
+
+
+
 
 const HomeComponent = () => {
   return (
     <div className="section_home">
-        <h1>Home</h1>
+      <Container>
+        <Row>
+        <Col>
+          <Image src="/logo.png" width={500} height={500} alt="Picture of the author"/>
+        </Col>
+        <Col>
+        
+        </Col>
+        </Row>
+      </Container>
     </div>
   )
 }
