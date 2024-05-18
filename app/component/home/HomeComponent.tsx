ae636cd3293f03021813845799ed9c6a84dc5ea9
eslamahmedabozei <your.email@example.com>
2024-05-18 19:56:28 +0300

    react

---
+++ app/component/home/HomeComponent.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './styleHome.css'
+
+const HomeComponent = () => {
+  return (
+    <div className="section_home">
+        <h1>Home</h1>
+    </div>
+  )
+}
+
+export default HomeComponent
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,13 @@
 import Image from "next/image";
 import Navbar from "./component/navbar/Navbar";
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import HomeComponent from "./component/home/HomeComponent";
 
 export default function Home() {
   return (
     <div>
       <Navbar />
+      <HomeComponent />
     </div>
   );
 }
