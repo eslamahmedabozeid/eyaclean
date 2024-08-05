0ec2b9055de521d2b2f013bb7223a7301882f57f
Eslam Ahmed <dev.eslamahmedabozeid@gmail.com>
2024-08-05 21:19:53 +0300

    react

--- app/component/home/HomeComponent.tsx
+++ app/component/home/HomeComponent.tsx
@@ -1,25 +1,94 @@
+/* eslint-disable jsx-a11y/alt-text */
+/* eslint-disable @next/next/no-img-element */
 import React from 'react'
 import './styleHome.css'
 import {Container, Row, Col} from 'react-bootstrap';
 import Image from 'next/image';
+import  './styleHome.css'
 
 
 
 
 const HomeComponent = () => {
   return (
+    <div>
     <div className="section_home">
-      <Container>
-        <Row>
-        <Col>
-          <Image src="/logo.png" width={500} height={500} alt="Picture of the author"/>
-        </Col>
-        <Col>
-        
-        </Col>
-        </Row>
-      </Container>
-    </div>
+    <main className="main">
+      <div className="hero">
+
+              <video className="video" loop muted autoPlay playsInline>
+                <source src="https://55prod.mtjrsahl-ksa.com/WhatsApp.mp4" type="video/mp4" />
+              </video>
+
+        <div className="overlay">
+          <div className="Enjoying">
+            <span>Enjoying</span> 
+            <img src="/imgs/FZFFBmXoAA3sjs 1.png" /> 
+            </div>
+            <div className="Enjoying">
+            <img src="/imgs/FZFFBmXoAA3sjs 1.png" /> 
+            <span>balance</span> 
+            </div>
+          <p className="lift_time">In Life</p>
+          <p className="bragraph">The healthiest and freshest food delivered to your doorsteps</p>
+          <div className='text_center'>
+          <button className="DownloadNow">
+            <img 
+          src="/imgs/download-withe.png" 
+          alt="Picture of the author" 
+          style={{ width: 'auto', height: 'auto' }} 
+          />
+           Download now</button>
+        </div>
+        </div>
+      </div>
+    </main>
+  </div>
+    <div className='second_section'>
+          <Container>
+            <div className='second_aline'>
+            <Row>
+              <Col className="aline_flxed">
+                <div className="data_text">
+                  <h1>Download Our <br/>
+                  Mobile App</h1>
+                  <p>Order your favorite meals on the go. Available on the App Store and Google Play.</p>
+                  <div className='fixed_section'>
+                  <img src='/imgs/Apple.png' />
+                  <img src='/imgs/Google.png' />
+                  </div>
+                </div>
+              </Col>
+              <Col>
+                <img className='image_mobile' src='/imgs/Frame54.png' />
+              </Col>
+            </Row>
+            </div>
+          </Container>
+      </div>
+
+      <div className='three_section'>
+        <div className='images'>
+        <img src='/imgs/Frame52.png' />
+        <img src='/imgs/Union.png' />
+        </div>
+        <div className="inner_section">
+          <Container>
+            <Row>
+              <Col>
+              <div className='text_line'>
+               <p>Salads</p>
+               <h1>The Fastes Delivery To Your Favorite Food</h1>
+               </div>
+              </Col>
+              <Col>
+                <img className='posishion' src="/imgs/Vector.png" />
+              </Col>
+            </Row>
+            </Container>
+        </div>
+      </div>
+  </div>
   )
 }
 
--- app/component/navbar/Navbar.tsx
+++ app/component/navbar/Navbar.tsx
@@ -1,28 +1,36 @@
+/* eslint-disable @next/next/no-img-element */
 import React from 'react'
 import Image from 'next/image'
 import  './styleNav.css'
-import {Container, Row, Col} from 'react-bootstrap';
-
+import {Container, Row, Col, Button} from 'react-bootstrap';
 
 function Navbar() {
   return (
     <div>
-      <Container>
+      <Container className='navBar'>
       <div className="flexed">
+      <Image src="/Logo.png" width={127.64} 
+      height={31.456} alt="Picture of the author"/>
+
         <ul className='nav_bar'>
-          <li>تواصل معنا</li>
-          <li>سياسة الخصوصية</li>
-          <li>سياسة الاسترجاع</li>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Menu</li>
+          <li>Contact Us</li>
         </ul>
       <div className="langu">
-        <p>الإنجليزية</p>
+        <Button className='downloadButton'>       
+            <img 
+          src="/imgs/download-04.png" 
+          alt="Picture of the author" 
+          style={{ width: 'auto', height: 'auto' }} 
+          />
+        Download App</Button>
       </div>
       </div>
       </Container>
 
-      <div className='logo'>
-        <Image src="/logo.png" width={500} height={500} alt="Picture of the author"/>
-      </div>
+     
 
 
     </div>
