0c446bcd17cdc95582818a872e5b1e2cb8e148b9
Eslam Ahmed <dev.eslamahmedabozeid@gmail.com>
2024-08-07 18:26:51 +0300

    react

--- app/component/navbar/Navbar.tsx
+++ app/component/navbar/Navbar.tsx
@@ -1,4 +1,5 @@
 /* eslint-disable @next/next/no-img-element */
+'use client'
 import React from 'react'
 import Image from 'next/image'
 import  './styleNav.css'
@@ -12,34 +13,60 @@ import NavDropdown from 'react-bootstrap/NavDropdown';
 
 function NavbarComponent() {
   return (
-    <div>
-      <Container className='navBar'>
-      <div className="flexed">
-      <Image src="/Logo.png" width={127.64} 
-      height={31.456} alt="Picture of the author"/>
+    // <div>
+    //   <Container className='navBar'>
+    //   <div className="flexed">
+    //   <Image src="/Logo.png" width={127.64} 
+    //   height={31.456} alt="Picture of the author"/>
 
-        <ul className='nav_bar'>
-          <li><Link href="/"> Home </Link></li>
-          <li><Link href="/pages/about"> About Us </Link></li>
-          <li>Menu</li>
-          <li>Contact Us</li>
-        </ul>
-      <div className="langu">
-        <Button className='downloadButton'>       
-            <img 
-          src="/imgs/download-04.png" 
-          alt="Picture of the author" 
-          style={{ width: 'auto', height: 'auto' }} 
-          />
-        Download App</Button>
-      </div>
-      </div>
-      </Container>
+    //     <ul className='nav_bar'>
+    //       <li><Link href="/"> Home </Link></li>
+    //       <li><Link href="/pages/about"> About Us </Link></li>
+    //       <li>Menu</li>
+    //       <li>Contact Us</li>
+    //     </ul>
+    //   <div className="langu">
+    //     <Button className='downloadButton'>       
+    //         <img 
+    //       src="/imgs/download-04.png" 
+    //       alt="Picture of the author" 
+    //       style={{ width: 'auto', height: 'auto' }} 
+    //       />
+    //     Download App</Button>
+    //   </div>
+    //   </div>
+    //   </Container>
 
      
 
 
-    </div>
+    // </div>
+
+    <Navbar expand="lg" className="navBar">
+      <Container>
+        <Navbar.Brand href="#home"><Image src="/Logo.png" width={127.64} 
+      height={31.456} alt="Picture of the author"/></Navbar.Brand>
+
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="nav_bar">
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="/pages/about">About Us</Nav.Link>
+            <Nav.Link href="#link">Menu</Nav.Link>
+            <Nav.Link href="#link">Contact Us</Nav.Link>
+          </Nav>
+          <div className="langu">
+         <Button className='downloadButton'>       
+            <img 
+           src="/imgs/download-04.png" 
+           alt="Picture of the author" 
+           style={{ width: 'auto', height: 'auto' }} 
+           />
+         Download App</Button>
+       </div>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   )
 }
 
