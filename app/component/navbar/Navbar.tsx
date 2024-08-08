94e6d8335a35533fdfd9f4bf66f6356dc12fa057
Eslam Ahmed <dev.eslamahmedabozeid@gmail.com>
2024-08-08 19:48:54 +0300

    angular

--- app/component/navbar/Navbar.tsx
+++ app/component/navbar/Navbar.tsx
@@ -13,35 +13,6 @@ import NavDropdown from 'react-bootstrap/NavDropdown';
 
 function NavbarComponent() {
   return (
-    // <div>
-    //   <Container className='navBar'>
-    //   <div className="flexed">
-    //   <Image src="/Logo.png" width={127.64} 
-    //   height={31.456} alt="Picture of the author"/>
-
-    //     <ul className='nav_bar'>
-    //       <li><Link href="/"> Home </Link></li>
-    //       <li><Link href="/pages/about"> About Us </Link></li>
-    //       <li>Menu</li>
-    //       <li>Contact Us</li>
-    //     </ul>
-    //   <div className="langu">
-    //     <Button className='downloadButton'>       
-    //         <img 
-    //       src="/imgs/download-04.png" 
-    //       alt="Picture of the author" 
-    //       style={{ width: 'auto', height: 'auto' }} 
-    //       />
-    //     Download App</Button>
-    //   </div>
-    //   </div>
-    //   </Container>
-
-     
-
-
-    // </div>
-
     <Navbar expand="lg" className="navBar">
       <Container>
         <Navbar.Brand href="#home"><Image src="/Logo.png" width={127.64} 
